7e94a955115e45651bd9d238f93b150b6ceb0979
shastaxc <shastaxc@gmail.com>
2024-07-14 10:23:53 -0400

    Add support for single item pops

--- AbysseaPopper.lua
+++ AbysseaPopper.lua
@@ -32,6 +32,9 @@ _addon.commands = {'ap','apop','abysseapopper'}
 -------------------------------------------------------------------------------
 -- Includes/imports
 -------------------------------------------------------------------------------
+require('tables')
+require('sets')
+
 res = require('resources')
 packets = require('packets')
 -- inspect = require('inspect')
@@ -52,10 +55,57 @@ inline_green = '\\cs(0,255,0)'
 inline_blue = '\\cs(0,0,255)'
 inline_gray = '\\cs(170,170,170)'
 
+abyssea_areas = S{
+  'Abyssea - Konschtat',
+  'Abyssea - Tahrongi',
+  'Abyssea - La Theine',
+  'Abyssea - Attohwa',
+  'Abyssea - Misareaux',
+  'Abyssea - Vunkerl',
+  'Abyssea - Altepa',
+  'Abyssea - Uleguerand',
+  'Abyssea - Grauberg',
+}
+
+-- Index by spawn point ID
+pop_info = T{
+  -- La Theine Liege
+  [17318479] = {id=17318479, required_items=S{2897}, required_key_items=S{}},
+  -- Baba Yaga
+  [17318480] = {id=17318480, required_items=S{2898}, required_key_items=S{}},
+  -- Carabosse
+  [17318486] = {id=17318489, required_items=S{}, required_key_items=S{1485, 1486}},
+  [17318489] = {id=17318489, required_items=S{}, required_key_items=S{1485, 1486}},
+  [17318492] = {id=17318489, required_items=S{}, required_key_items=S{1485, 1486}},
+}
+-- Replace required_items and required_key_items with more detailed objects
+for entry in pop_info:it() do
+  local req_items = entry.required_items:copy(true)
+  entry.required_items = S{}
+  for item_id in req_items:it() do
+    local res_item = res.items[item_id]
+    if res_item then
+      local new_item = {id=res_item.id, en=res_item.en, enl=res_item.enl, ja=res_item.ja, jal=res_item.jal}
+      entry.required_items:append(new_item)
+    end
+  end
+
+  local req_ki = entry.required_key_items:copy(true)
+  entry.required_key_items = S{}
+  for ki_id in req_ki:it() do
+    local res_item = res.key_items[ki_id]
+    if res_item then
+      local new_item = {id=res_item.id, en=res_item.en, ja=res_item.ja}
+      entry.required_key_items:append(new_item)
+    end
+  end
+end
+
 function init(force_init)
   player = {} -- Player status
-  tracker = {} -- Tracks enemies' step debuffs, keyed by enemy actor ID
+  world = {} -- World info
   update_player_info()
+  refresh_ffxi_info()
 end
 
 -- Update player info
@@ -64,19 +114,83 @@ function update_player_info()
   if player_info then
     player.id = player_info.id
     player.name = player_info.name
-    player.main_job = player_info.main_job
-    player.main_job_level = player_info.main_job_level
-    player.sub_job = player_info.sub_job
-    player.sub_job_level = player_info.sub_job_level
-    player.merits = player_info.merits
-    player.job_points = player_info.job_points
   end
+end
+
+function refresh_ffxi_info()
+  local info = windower.ffxi.get_info()
+  local zone = info['zone']
+  if zone and res.zones[zone] then
+    world.zone = res.zones[zone].en
+    world.area = world.zone
+  end
+end
+
+function get_target()
+  local npc = windower.ffxi.get_mob_by_target('t')
+  if npc then
+    if math.sqrt(npc.distance) < 6 then
+      return npc
+    else
+      windower.add_to_chat(001, chat_red..'AbysseaPopper: Target out of range.')
+    end
+  end
+end
+
+-- Attempt to pop NM based on current target
+function pop_target()
+  -- Get target info
+  local t = get_target()
+  if t then
+    local info = pop_info[t.id]
+    if info then
+      -- If NM requires items to pop, attempt to trade
+      if info.required_items:length() > 0 then
+        if info.required_items:length() == 1 then
+          -- If only 1 required item, we can use in-game command to pop
+          local req_item = info.required_items[1]
+          send_command('@input /item "'..req_item.en..'" <t>')
+        end
+      elseif info.required_key_items:length() > 0 then
+        -- If NM requires key items to pop, deal with the popup menu
+      end
+    end
+  end
+end
+
+function is_in_abyssea()
+  if abyssea_areas.contains(world.area) then
+    return true
+  end
+
+  return false
+end
+
+-- Takes optional target_id
+function get_pop_info(target_id)
+  if is_in_abyssea() then
+    if target_id then
+      return pop_info[target_id]
+    else
+      local target = get_target()
+      if target and pop_info[target.id] then
+        return pop_info[target.id]
+      end
+    end
+  end
+
+  return nil
 end
 
 windower.register_event('load', function()
   if windower.ffxi.get_player() then
     init()
   end
+end)
+
+windower.register_event('zone change', function(new_id, old_id)
+  world.zone = res.zones[new_id].en
+  world.area = world.zone
 end)
 
 windower.register_event('addon command', function(cmd, ...)
@@ -91,21 +205,30 @@ windower.register_event('addon command', function(cmd, ...)
     if S{'reload', 'r'}:contains(cmd) then
       windower.send_command('lua r abysseapopper')
       windower.add_to_chat(001, chat_d_blue..'AbysseaPopper: Reloading.')
+    elseif S{'trade', 'pop', 'spawn'}:contains(cmd) then
+      -- If in Abyssea, attempt to pop target
+      if is_in_abyssea() then
+        pop_target()
+      end
+    elseif 'info' == cmd then
+      local pop_info = get_pop_info()
+      if pop_info then
+        windower.add_to_chat(001, chat_d_blue..'AbysseaPopper: '..pop_info.name..' pops here.')
+      else
+        windower.add_to_chat(001, chat_d_blue..'AbysseaPopper: This is not a supported pop location.')
+      end
     elseif 'test' == cmd then
     elseif 'help' == cmd then
       windower.add_to_chat(6, ' ')
       windower.add_to_chat(6, chat_d_blue.. 'AbysseaPopper Commands available:' )
+      windower.add_to_chat(6, chat_l_blue..	'//ap help ' .. chat_white .. ': Display this help menu again')
       windower.add_to_chat(6, chat_l_blue..	'//ap r' .. chat_white .. ': Reload addon')
-      windower.add_to_chat(6, chat_l_blue..	'//met vis ' .. chat_white .. ': Toggle UI visibility')
-      windower.add_to_chat(6, chat_l_blue..	'//met show ' .. chat_white .. ': Show UI')
-      windower.add_to_chat(6, chat_l_blue..	'//met hide ' .. chat_white .. ': Hide UI')
-      windower.add_to_chat(6, chat_l_blue..	'//met resetpos ' .. chat_white .. ': Reset position of UI to default')
-      windower.add_to_chat(6, chat_l_blue..	'//met jobs ' .. chat_white .. ': Display show/hide based on job being DNC or not.')
-      windower.add_to_chat(6, chat_l_blue..	'//met help ' .. chat_white .. ': Display this help menu again')
+      windower.add_to_chat(6, chat_l_blue..	'//ap pop' .. chat_white .. ': Pop NM at targeted point')
+      windower.add_to_chat(6, chat_l_blue..	'//ap info' .. chat_white .. ': Report which NM pops at targeted point')
     else
-      windower.send_command('met help')
+      windower.send_command('abysseapopper help')
     end
   else
-    windower.send_command('met help')
+    windower.send_command('abysseapopper help')
   end
 end)
